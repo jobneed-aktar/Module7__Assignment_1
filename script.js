f69b56591ae02b257ec949f853a65a5c5a8b9a53
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-15 06:19:38 +0600

    Add two number function added

---
+++ script.js
@@ -0,0 +1,63 @@
+// Add two sum using regular function
+/*
+    system__1
+*/
+
+// function add(x,y) {
+//     let result = x + y;
+//     console.log(result)
+// }
+// add(10,20);
+
+
+
+/*
+    system__2
+*/
+
+// function add(x,y) {
+//     return x + y;
+// }
+// console.log(add(10,20));
+
+
+
+/*
+    system__3
+*/
+
+// function add(x,y) {
+//     return x + y;
+// }
+// let totalResult = add(10,20);
+// console.log(totalResult);
+
+
+
+/*
+    system__4
+*/
+
+// function add(x,y) {
+//     return x + y;
+// }
+// let totalResult = add(10,20) + add(20,30) + add(30,40);
+// console.log(totalResult);
+
+
+
+/*
+    system__5
+*/
+
+// function add(x,y) {
+//     let sum = x + y;
+//     let multiply = x * y;
+//     return sum + multiply;
+// }
+// let totalResult = add(10,20);
+// console.log(totalResult);
+
+
+
+// Even or Odd number checker
